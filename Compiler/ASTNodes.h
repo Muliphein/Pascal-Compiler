6393f4f2b6ab1df860d3ad0e9ccace3d70f92dfa
Wang Xulong <3052857822@qq.com>
2021-05-28 21:41:43 +0800

    ASTNodes BinaryOp SystemCall

---
+++ Compiler/ASTNodes.h
@@ -0,0 +1,6 @@
+#ifndef __ASTNODES_H__
+#define __ASTNODES_H__
+
+#include "BinaryOp.h"
+
+#endif
---
+++ Compiler/BinaryOp.h
@@ -0,0 +1,21 @@
+#ifndef __BINARYOP_H__
+#define __BINARYOP_H__
+
+enum BinaryOper{
+    GE,
+    GT,
+    LE,
+    LT,
+    EQUAL,
+    UNEQUAL,
+    OR,
+    AND,
+    MUL,
+    DIVISION,
+    DIV,
+    MOD,
+    PLUS,
+    MINUS
+};
+
+#endif
